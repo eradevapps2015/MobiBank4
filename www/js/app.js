d6f71bbdd2e7d9bd17dbacaaec8cfe49a3db7ebd
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-04-30 21:21:59 +0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,7 @@ angular.module('starter', ['ionic' ,'starter.controllers','starter.directives','
 
 .run(function($ionicPlatform,  $ionicPopup) {
   $ionicPlatform.ready(function() {
+  	alert("Device ready In add");
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
